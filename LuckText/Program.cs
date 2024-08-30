cd313b91f0f1850e317718d7db7d29018a6eec01
Mikael Bergström <mikael.bergstrom@ga.ntig.se>
2024-08-30 13:57:55 +0200

    Första

---
+++ LuckText/Program.cs
@@ -0,0 +1,14 @@
+﻿string name;
+
+Console.ForegroundColor = ConsoleColor.Green;
+Console.BackgroundColor = ConsoleColor.Cyan;
+Console.Clear();
+
+Console.WriteLine("Skriv in ditt namn: ");
+name = Console.ReadLine();
+
+Console.ForegroundColor = ConsoleColor.Blue;
+// Console.WriteLine("Hello " + name + "!");
+Console.WriteLine($"Hello {name}!");
+
+Console.ReadLine();
